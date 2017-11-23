5b9c56027d24d49404466c88067d988d5c7593e3
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-24 00:07:33 +0900

    財産モデルのtokenをユニーク制約に

--- db/migrate/20171123145627_create_properties.rb
+++ db/migrate/20171123145627_create_properties.rb
@@ -2,7 +2,7 @@ class CreateProperties < ActiveRecord::Migration[5.1]
   def change
     create_table :properties do |t|
       t.string :name, null: false
-      t.string :token, null: false
+      t.string :token, null: false, unique: true
       t.boolean :is_lost, default: true
       t.integer :lost_id
 
