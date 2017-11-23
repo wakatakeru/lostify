54317066e2d15a432b2613ea97085786d002fc66
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-24 00:12:33 +0900

    is_lostの初期値を修正

--- db/migrate/20171123145627_create_properties.rb
+++ db/migrate/20171123145627_create_properties.rb
@@ -3,7 +3,7 @@ class CreateProperties < ActiveRecord::Migration[5.1]
     create_table :properties do |t|
       t.string :name, null: false
       t.string :token, null: false, unique: true
-      t.boolean :is_lost, default: true
+      t.boolean :is_lost, default: false
       t.integer :lost_id
 
       t.timestamps
