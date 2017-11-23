b199982c57d8ad6a013932b2f9d6046db0ec7e5d
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-24 00:13:59 +0900

    schemaが更新された

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20171123145839) do
   create_table "properties", force: :cascade do |t|
     t.string "name", null: false
     t.string "token", null: false
-    t.boolean "is_lost", default: true
+    t.boolean "is_lost", default: false
     t.integer "lost_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
