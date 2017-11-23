2862934cadeddaa9bd36381b7beda90e0b3312cb
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-24 06:15:42 +0900

    QRコードを書き換えられるようにした

--- app/controllers/lost_controller.rb
+++ app/controllers/lost_controller.rb
@@ -12,26 +12,31 @@ class LostController < ApplicationController
     token = params['token']
     
     property = Property.where(token: token).first
-    property_name = property.name
 
-    
-    user_email = User.find(property.user_id).email
-    
-    # TODO: エラーハンドリング
-    @lost.current_location = params['lost']['current_location']
-    @lost.pickup_location = params['lost']['pickup_location']
-    @lost.property_id = property.id
-    @lost.save
+    if !(property.present?)
+      flash[:danger] = "このQRコードは無効です" 
+      redirect_to lost_index_path
+    else
+      property_name = property.name
 
-    property.is_lost = true
-    property.lost_id = @lost.id
-    current_loc = @lost.current_location
-    pickup_loc = @lost.pickup_location
-    property.save
+      user_email = User.find(property.user_id).email
+      
+      # TODO: エラーハンドリング
+      @lost.current_location = params['lost']['current_location']
+      @lost.pickup_location = params['lost']['pickup_location']
+      @lost.property_id = property.id
+      @lost.save
 
-    #ReportingMailer.report_email(user, property, @lost).deliver
-    ReportingWorker.perform_async(user_email, property_name, current_loc, pickup_loc)
-    
-    redirect_to lost_index_path
+      property.is_lost = true
+      property.lost_id = @lost.id
+      current_loc = @lost.current_location
+      pickup_loc = @lost.pickup_location
+      property.save
+
+      #ReportingMailer.report_email(user, property, @lost).deliver
+      ReportingWorker.perform_async(user_email, property_name, current_loc, pickup_loc)
+      
+      redirect_to lost_index_path
+    end
   end
 end
