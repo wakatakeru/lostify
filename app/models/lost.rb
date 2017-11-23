2723d02c2c099ca1d38a5fded3bf6348449be487
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-24 00:20:31 +0900

    多重度を設定

--- app/models/lost.rb
+++ app/models/lost.rb
@@ -1,3 +1,4 @@
 class Lost < ApplicationRecord
+  has_one :property
   validates :current_location, presence: true
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,6 @@
 class Property < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true
   validates :token, uniqueness: true, presence: true
   validates :is_lost, inclusion: { in: [true, false] }
